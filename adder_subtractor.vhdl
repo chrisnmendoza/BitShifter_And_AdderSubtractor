c620c95f5d33a5db8fcee15e1f909cadc50f780c
Chris <mendozac@udel.edu>
2021-04-22 15:16:49 -0400

    made adder_subtractor into a process to conform to directions

--- adder_subtractor.vhdl
+++ adder_subtractor.vhdl
@@ -15,21 +15,23 @@ architecture behav of adder_subtractor is
     signal lowerModifiedInput : std_logic_vector(3 downto 0);  --the possibly inverted version of lowerInput
     signal carry : std_logic_vector(4 downto 0); --this is the carry-in and carry-out
 begin
-    lowerModifiedInput(0) <= lowerInput(0) xor sel; --xor sel will successfully invert if sel == 1 (subtraction)
-    lowerModifiedInput(1) <= lowerInput(1) xor sel;
-    lowerModifiedInput(2) <= lowerInput(2) xor sel;
-    lowerModifiedInput(3) <= lowerInput(3) xor sel;
-    carry(0) <= sel; --when sel == 1 this signifies subtraction, and according to 2's complement must add 1 to LSB
-    carry(1) <= ((upperInput(0) and carry(0)) or (upperInput(0) and lowerModifiedInput(0))) or (lowerModifiedInput(0) and carry(0));
-    carry(2) <= ((upperInput(1) and carry(1)) or (upperInput(1) and lowerModifiedInput(1))) or (lowerModifiedInput(1) and carry(1));
-    carry(3) <= ((upperInput(2) and carry(2)) or (upperInput(2) and lowerModifiedInput(2))) or (lowerModifiedInput(2) and carry(2));
-    carry(4) <= (upperInput(3) xor lowerModifiedInput(3)) xor carry(3);	--this carry 4 will be used to check overflow/underflow, this is the same as O(3)
-    O(0) <= (upperInput(0) xor lowerModifiedInput(0)) xor carry(0); 		
-    O(1) <= (upperInput(1) xor lowerModifiedInput(1)) xor carry(1);	
-    O(2) <= (upperInput(2) xor lowerModifiedInput(2)) xor carry(2);	
-    O(3) <= (upperInput(3) xor lowerModifiedInput(3)) xor carry(3);
-    overflow <= (not (upperInput(3) xor lowerModifiedInput(3))) and (carry(4)) and (upperInput(3) xor carry(4));
-    underflow <= (not (upperInput(3) xor lowerModifiedInput(3))) and (not carry(4)) and (upperInput(3) xor carry(4));
-
+    process(upperInput, lowerInput, sel, carry, lowerModifiedInput) is
+    begin
+        lowerModifiedInput(0) <= lowerInput(0) xor sel; --xor sel will successfully invert if sel == 1 (subtraction)
+        lowerModifiedInput(1) <= lowerInput(1) xor sel;
+        lowerModifiedInput(2) <= lowerInput(2) xor sel;
+        lowerModifiedInput(3) <= lowerInput(3) xor sel;
+        carry(0) <= sel; --when sel == 1 this signifies subtraction, and according to 2's complement must add 1 to LSB
+        carry(1) <= ((upperInput(0) and carry(0)) or (upperInput(0) and lowerModifiedInput(0))) or (lowerModifiedInput(0) and carry(0));
+        carry(2) <= ((upperInput(1) and carry(1)) or (upperInput(1) and lowerModifiedInput(1))) or (lowerModifiedInput(1) and carry(1));
+        carry(3) <= ((upperInput(2) and carry(2)) or (upperInput(2) and lowerModifiedInput(2))) or (lowerModifiedInput(2) and carry(2));
+        carry(4) <= (upperInput(3) xor lowerModifiedInput(3)) xor carry(3);	--this carry 4 will be used to check overflow/underflow, this is the same as O(3)
+        O(0) <= (upperInput(0) xor lowerModifiedInput(0)) xor carry(0); 		
+        O(1) <= (upperInput(1) xor lowerModifiedInput(1)) xor carry(1);	
+        O(2) <= (upperInput(2) xor lowerModifiedInput(2)) xor carry(2);	
+        O(3) <= (upperInput(3) xor lowerModifiedInput(3)) xor carry(3);
+        overflow <= (not (upperInput(3) xor lowerModifiedInput(3))) and (carry(4)) and (upperInput(3) xor carry(4));
+        underflow <= (not (upperInput(3) xor lowerModifiedInput(3))) and (not carry(4)) and (upperInput(3) xor carry(4));
+    end process;
 end behav;
 
