aeaf8289462c2a1542608323014082de6025aa95
Chris <mendozac@udel.edu>
2021-04-25 19:41:29 -0400

    timing issues

--- shift_reg_8bit.vhdl
+++ shift_reg_8bit.vhdl
@@ -4,9 +4,9 @@ use ieee.std_logic_1164.all;
 entity shift_reg_8bit is
 port(	I_8bit:	in std_logic_vector (7 downto 0); -- for loading
 		I_SHIFT_IN_8bit: in std_logic; -- shifted in bit for both left and right
-		sel:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
-		clock:		in std_logic; -- positive level triggering in problem 3
-		enable:		in std_logic; -- 0: don't do anything; 1: shift_reg is enabled
+		sel_8bit:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
+		clock_8bit:		in std_logic; -- positive level triggering in problem 3
+		enable_8bit:		in std_logic; -- 0: don't do anything; 1: shift_reg is enabled
 		O_8bit:	out std_logic_vector(7 downto 0) -- output the current register content. HINT: should be combinational.
 		-- SHIFT_OUT : out std_logic;
 );
@@ -25,35 +25,56 @@ port (	I:	in std_logic_vector (3 downto 0);
 );
 end component;
 
-signal values : std_logic_vector (7 downto 0);	--saved values before it gets loaded into O
+signal values_8bit : std_logic_vector (7 downto 0);	--saved values before it gets loaded into O
 signal inshift0: std_logic;
 signal inshift1: std_logic;
 begin
 
 --  Component instantiation.
-shift_reg_0: shift_reg port map (I => I_8bit(7 downto 4), I_SHIFT_IN => inshift0, sel => sel, clock => clock, enable => enable, O => O_8bit(7 downto 4)); --left 4 bits
-shift_reg_1: shift_reg port map (I => I_8bit(3 downto 0), I_SHIFT_IN => inshift1, sel => sel, clock => clock, enable => enable, O => O_8bit(3 downto 0)); --right 4 bits
+shift_reg_0: shift_reg port map (I => I_8bit(7 downto 4), I_SHIFT_IN => inshift0, sel => sel_8bit, clock => clock_8bit, enable => enable_8bit, O => O_8bit(7 downto 4)); --left 4 bits
+shift_reg_1: shift_reg port map (I => I_8bit(3 downto 0), I_SHIFT_IN => inshift1, sel => sel_8bit, clock => clock_8bit, enable => enable_8bit, O => O_8bit(3 downto 0)); --right 4 bits
 
-process(I_8bit, I_SHIFT_IN_8bit, sel, enable, clock) is
+process(I_8bit, I_SHIFT_IN_8bit, sel_8bit, enable_8bit, clock_8bit) is
 begin
-	if (clock='1' and enable='1') then
-		if (sel="00") then
+	if (clock_8bit='1' and enable_8bit='1') then
+		if (sel_8bit="00") then
+			inshift0 <= I_SHIFT_IN_8bit;
+            inshift1 <= I_SHIFT_IN_8bit;
 			-- do nothing
             --O_8bit <= values;
 
-		elsif (sel ="01") then --left shift
+		elsif (sel_8bit ="01") then --left shift
             --write to a mux values(3) and send it in I_SHIFT_IN
+            inshift0 <= values_8bit(3);
             inshift1 <= I_SHIFT_IN_8bit;
-            inshift0 <= values(3);
+			values_8bit(7) <= values_8bit(6);
+			values_8bit(6) <= values_8bit(5);
+			values_8bit(5) <= values_8bit(4);
+			values_8bit(4) <= values_8bit(3);
+			values_8bit(3) <= values_8bit(2);
+			values_8bit(2) <= values_8bit(1);
+			values_8bit(1) <= values_8bit(0);
+			values_8bit(0) <= I_SHIFT_IN_8bit;
             --O_8bit <= values;
             
-		elsif (sel="10") then --right shift
-            inshift0 <= I_SHIFT_IN_8bit;
-            inshift1 <= values(4);
+		elsif (sel_8bit="10") then --right shift
+			inshift0 <= I_SHIFT_IN_8bit;
+            inshift1 <= values_8bit(4);
+			values_8bit(0) <= values_8bit(1);
+			values_8bit(1) <= values_8bit(2);
+			values_8bit(2) <= values_8bit(3);
+			values_8bit(3) <= values_8bit(4);
+			values_8bit(4) <= values_8bit(5);
+			values_8bit(5) <= values_8bit(6);
+			values_8bit(6) <= values_8bit(7);
+			values_8bit(7) <= I_SHIFT_IN_8bit;
+
             --O_8bit <= values;
-		elsif (sel="11") then
+		elsif (sel_8bit="11") then
+			inshift0 <= I_SHIFT_IN_8bit;
+            inshift1 <= I_SHIFT_IN_8bit;
 			--O_8bit <= I_8bit;
-            values <= I_8bit;
+            values_8bit <= I_8bit;
 		end if;
 		
 		
--- shift_reg_8bit_tb.vhdl
+++ shift_reg_8bit_tb.vhdl
@@ -10,9 +10,9 @@ architecture behav of shift_reg_8bit_tb is
 component shift_reg_8bit
 port (	I_8bit:	in std_logic_vector (7 downto 0);
 		I_SHIFT_IN_8bit: in std_logic;
-		sel:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
-		clock:		in std_logic; 
-		enable:		in std_logic;
+		sel_8bit:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
+		clock_8bit:		in std_logic; 
+		enable_8bit:		in std_logic;
 		O_8bit:	out std_logic_vector(7 downto 0)
 );
 end component;
@@ -23,7 +23,7 @@ signal i_shift_in, clk, enable : std_logic;
 signal sel : std_logic_vector(1 downto 0);
 begin
 --  Component instantiation.
-shift_reg_8bit_0: shift_reg_8bit port map (I_8bit => i, I_SHIFT_IN_8bit => i_shift_in, sel => sel, clock => clk, enable => enable, O_8bit => o);
+shift_reg_8bit_0: shift_reg_8bit port map (I_8bit => i, I_SHIFT_IN_8bit => i_shift_in, sel_8bit => sel, clock_8bit => clk, enable_8bit => enable, O_8bit => o);
 
 --  This process does the real job.
 process
@@ -40,9 +40,18 @@ type pattern_array is array (natural range <>) of pattern_type;
 constant patterns : pattern_array := --WRONG test vectors. replace with your own.
 
 (("11000000", '0', '1', '1', "11", "11000000"),
+("11000011", '0', '1', '1', "11", "11000011"),
+("11111111", '0', '1', '1', "10", "01100001"),
+("00000000", '0', '1', '1', "10", "00110000"),
+("11111111", '0', '1', '1', "10", "00011000"),
+("00000000", '0', '1', '1', "10", "00001100"),
+("00000000", '0', '1', '1', "00", "00001100"),
+("11111111", '0', '1', '1', "10", "00000110"),
+("00000000", '0', '1', '1', "10", "00000011"),
+("11111111", '0', '1', '1', "10", "00000001"),
+("00000000", '0', '1', '1', "10", "00000000"),
 ("11000001", '0', '1', '1', "11", "11000001"),
-("11000001", '0', '1', '1', "10", "01100000"),
-("11000001", '0', '1', '1', "00", "01100000")); -- Need two vectors to simulate an edge.
+("11000001", '1', '1', '1', "11", "11000001")); -- Need two vectors to simulate an edge.
 begin
 --  Check each pattern.
 for n in patterns'range loop
